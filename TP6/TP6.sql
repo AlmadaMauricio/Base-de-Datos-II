4b118a5f3140c12955ea9f6fbc7b6d589553b6ff
AlmadaMauricio <110863446+AlmadaMauricio@users.noreply.github.com>
2025-09-30 20:16:45 -0300

    subiendo enunciados

--- TP6/TP6.sql
+++ TP6/TP6.sql
@@ -8,8 +8,12 @@ Where A.Tamaño > (Select AVG(Tamaño) From Archivos);
 /*2 Los usuarios indicando apellido y nombre que no sean dueños de ningún archivo con extensión 'zip'.*/
 Select U.Apellido, U.Nombre 
 From Usuarios U 
-Left Join Archivos A On U.IDUsuario = A.IDUsuarioDueño --falta terminar
- 
+Left Join Archivos A On U.IDUsuario = A.IDUsuarioDueño and A.Extension = 'zip'
+Where A.IDArchivo is null;
 
+/*3 Los usuarios indicando IDUsuario, apellido, nombre y tipo de usuario que no hayan creado ni modificado ningún archivo en el año actual.*/
+Select U.IDUsuario, U.Apellido, U.Nombre, TU.TipoUsuario
+From Usuarios U
+Where (Select IDUsuarioDueño From Archivos Where not exists )
 
 
