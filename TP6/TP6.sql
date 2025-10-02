2e1ba3209c7c02874773cf7ac4deac549b47468e
AlmadaMauricio <110863446+AlmadaMauricio@users.noreply.github.com>
2025-10-02 17:58:44 -0300

    subiendo enunciados resueltos

--- TP6/TP6.sql
+++ TP6/TP6.sql
@@ -12,8 +12,30 @@ Left Join Archivos A On U.IDUsuario = A.IDUsuarioDueño and A.Extension = 'zip'
 Where A.IDArchivo is null;
 
 /*3 Los usuarios indicando IDUsuario, apellido, nombre y tipo de usuario que no hayan creado ni modificado ningún archivo en el año actual.*/
-Select U.IDUsuario, U.Apellido, U.Nombre, TU.TipoUsuario
-From Usuarios U
-Where (Select IDUsuarioDueño From Archivos Where not exists )
+SELECT 
+    U.IDUsuario, 
+    U.Apellido, 
+    U.Nombre, 
+    TU.TipoUsuario
+FROM Usuarios U
+INNER JOIN TiposUsuario TU ON U.IDTipoUsuario = TU.IDTipoUsuario
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Archivos A
+    WHERE A.IDUsuarioDueño = U.IDUsuario
+      AND (
+          YEAR(A.FechaCreacion) = 2025
+          OR YEAR(A.FechaUltimaModificacion) = 2025
+      )
+);
+
+/*4 Los tipos de usuario que no registren usuario con archivos eliminados.*/
+Select TU.TipoUsuario
+From TiposUsuario TU
+Inner Join Usuarios U On TU.IDTipoUsuario = U.IDTipoUsuario
+Where not exists (select IDUsuarioDueño From Archivos A Where A.IDUsuarioDueño = U.IDUsuario And Eliminado = 1);
+
+
+
 
 
