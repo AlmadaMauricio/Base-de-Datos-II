0a7cc25a214a8be5381c574c8be07245b79ba73f
AlmadaMauricio <110863446+AlmadaMauricio@users.noreply.github.com>
2025-10-06 16:45:52 -0300

    subiendo enunciados resueltos de tp6

--- TP6/TP6.sql
+++ TP6/TP6.sql
@@ -58,7 +58,46 @@ From Archivos A
 Where A.Tamaño > (Select Min(Ar.Tamaño) From Archivos Ar Where Ar.Extension = 'zip');
 
 /*8 Por cada tipo de archivo indicar el tipo y la cantidad de archivos eliminados y la cantidad de archivos no eliminados.*/
-Select TA.TipoArchivo
+Select TA.TipoArchivo,
+sum(Case when A.Eliminado = 1 then 1 else 0 end) As 'Eliminado',
+sum(Case when A.Eliminado = 0 then 1 else 0 end) As 'No Eliminado'
 From TiposArchivos TA
+Inner Join Archivos A On TA.IDTipoArchivo = A.IDTipoArchivo
+GROUP BY Ta.TipoArchivo;
+
+/*9 Por cada usuario indicar el IDUsuario, el apellido, el nombre,
+la cantidad de archivos pequeños (menos de 20MB) y la cantidad de archivos grandes (20MBs o más)*/
+Select U.IDUsuario, U.Apellido, U.Nombre,
+Sum(Case when A.Tamaño < 20971520 Then 1 else 0 end) As Menor20MB,
+Sum(Case when A.Tamaño >= 20971520 then 1 else 0 end) As Mayor20MB
+From Usuarios U
+Inner Join Archivos A On U.IDUsuario = A.IDUsuarioDueño
+GROUP BY U.IDUsuario, U.Apellido, U.Nombre;
+
+/*10 Por cada usuario indicar el IDUsuario, el apellido, el nombre y la cantidad de archivos creados en el año 2022,
+la cantidad en el año 2023 y la cantidad creados en el 2024.*/
+Select U.IDUsuario, U.Apellido, U.Nombre,
+Sum(Case When YEAR(A.FechaCreacion) = 2022 Then 1 else 0 end) As Creado2022,
+Sum(Case When YEAR(A.FechaCreacion) = 2023 Then 1 else 0 end) As Creado2023,
+Sum(Case When YEAR(A.FechaCreacion) = 2024 Then 1 else 0 end) As Creado2024
+From Usuarios U
+Inner Join Archivos A On U.IDUsuario = A.IDUsuarioDueño
+GROUP BY U.IDUsuario, U.Apellido, U.Nombre;
+
+/*11 Los archivos que fueron compartidos con permiso de 'Comentario' pero no con permiso de 'Lectura'*/
+SELECT DISTINCT A.*
+FROM Archivos A
+INNER JOIN ArchivosCompartidos AC ON A.IDArchivo = AC.IDArchivo
+INNER JOIN Permisos P ON AC.IDPermiso = P.IDPermiso
+WHERE P.Nombre = 'Comentario'
+AND NOT EXISTS (SELECT 1
+FROM ArchivosCompartidos AC2
+INNER JOIN Permisos P2 ON AC2.IDPermiso = P2.IDPermiso
+WHERE AC2.IDArchivo = A.IDArchivo
+AND P2.Nombre = 'Lectura');
+
+/*12 Los tipos de archivos que registran más archivos eliminados que no eliminados.*/
+
+
 
 
