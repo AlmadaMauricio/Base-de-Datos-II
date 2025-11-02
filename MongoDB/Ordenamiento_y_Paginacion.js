01782e8fb5d275c15aafa7aef78fc3933db5f9c2
AlmadaMauricio <110863446+AlmadaMauricio@users.noreply.github.com>
2025-11-02 15:42:10 -0300

    subiendo ejercicios

--- MongoDB/Ordenamiento_y_Paginacion.js
+++ MongoDB/Ordenamiento_y_Paginacion.js
@@ -8,30 +8,30 @@ use('angeles_films');
 // PARTE 2 - INSERTAR DOCUMENTO EN COLLECTION -> peliculas
 // PARTE 3 - ORDENAMIENTO
 // 1- Traer todas las películas ordenadas por año de estreno, de más nueva a más vieja.
-//db.peliculas.find({}).sort({anio: 1})
+db.peliculas.find({}).sort({anio: 1})
 // 2- Listar las películas ordenadas por duración, de menor a mayor.
-//db.peliculas.find({}).sort({duracion: 1})
+db.peliculas.find({}).sort({duracion: 1})
 // 3- Mostrar todas las películas ordenadas alfabéticamente por título.
-//db.peliculas.find({}).sort({titulo: 1})
+db.peliculas.find({}).sort({titulo: 1})
 
 /*###############################################################################################*/
 
 // PARTE 4 - PAGINACIÓN
 // 1- Traer las primeras 3 películas de la colección.
-//db.peliculas.find({}).limit(3)
+db.peliculas.find({}).limit(3)
 // 2- Saltar las primeras 5 y mostrar las siguientes 2.
-//db.peliculas.find({}).skip(5).limit(2)
+db.peliculas.find({}).skip(5).limit(2)
 // 3- Mostrar la segunda página de resultados si cada página tiene 4 películas.
-//db.peliculas.find({}).skip(4).limit(4)
+db.peliculas.find({}).skip(4).limit(4)
 
 /*###############################################################################################*/
 
 // PARTE 5 - ORDENAMIENTO Y PAGINACIÓN
 // 1- Mostrar las 3 películas más largas, ordenadas de mayor a menor duración.
-//db.peliculas.find({}).sort({duracion: -1}).limit(3)
+db.peliculas.find({}).sort({duracion: -1}).limit(3)
 // 2- Listar 5 películas ordenadas por rating descendente, pero solo mostrar las 3 primeras.
-//db.peliculas.find({}).sort({rating: -1}).limit(3)
+db.peliculas.find({}).sort({rating: -1}).limit(3)
 // 3- Mostrar las primeras 2 películas de Animación ordenadas por año de estreno ascendente.
-//db.peliculas.find({genero: "Animación"}).sort({anio: 1}).limit(2)
+db.peliculas.find({genero: "Animación"}).sort({anio: 1}).limit(2)
 // 4- Traer películas ordenadas por título, pero mostrar solo desde la cuarta en adelante (salteando 3).
 db.peliculas.find({}).sort({titulo: 1}).skip(3)
